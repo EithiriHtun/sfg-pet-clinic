11960544ac156736963cb1644cd61ba0a2fed450
eithiri <eithiritun1997@gmail.com>
2020-05-25 15:45:32 +0630

    Create and update pet # 219

--- pet-clinic-data/src/main/java/spring/web/app/springwepapp/Model/PetType.java
+++ pet-clinic-data/src/main/java/spring/web/app/springwepapp/Model/PetType.java
@@ -15,8 +15,15 @@ import javax.persistence.Table;
 @Table(name = "types")
 public class PetType extends BaseEntity{
 
+    @Builder
+    public PetType(Long id, String name) {
+        super(id);
+        this.name = name;
+    }
+
     @Column(name = "name")
     private String name;
+
 
     @Override
     public String toString() {
