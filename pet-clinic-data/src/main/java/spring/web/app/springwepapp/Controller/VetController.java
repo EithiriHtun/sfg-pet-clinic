6a0d0b22905d15dca533eb374d59904079455580
eithiri <eithiritun1997@gmail.com>
2020-06-02 14:03:54 +0630

    Spring Pet Clinic-Implement Vets List View #244

--- pet-clinic-data/src/main/java/spring/web/app/springwepapp/Controller/VetController.java
+++ pet-clinic-data/src/main/java/spring/web/app/springwepapp/Controller/VetController.java
@@ -4,8 +4,13 @@ import org.dom4j.rule.Mode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import spring.web.app.springwepapp.Model.Vet;
 import spring.web.app.springwepapp.Services.VetService;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -21,6 +26,13 @@ public class VetController {
     public  String listVet(Model model){
         model.addAttribute("vets",vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody
+    Set<Vet> getVetsJson(){
+
+        return vetService.findAll();
     }
 
     }
